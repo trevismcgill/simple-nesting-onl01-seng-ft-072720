157c349526c339857008e0e157dfdb1cbc73386d
Trevis McGill <trevismcgill25@gmail.com>
2020-07-31 14:19:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,9 +62,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- yuki_hash = yukihiro_matsumoto: {
+ yuki_hash = {yukihiro_matsumoto: {
    known_for: "Ruby",
    languages: ["LISP", "C"]
+ }
  }
 
 	programmer_hash =
